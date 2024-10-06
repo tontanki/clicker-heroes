8c70617be15ce53e090425306244ad99c975be80
tontanki <dragonnight612@gmail.com>
2024-10-06 11:16:04 +0900

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>It's nice to meet</p>
+        </div>
+    );
+}
+
+export default App;
